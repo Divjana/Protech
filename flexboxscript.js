d88b8ab6765bdd92c6d2e3e8469f7200d3b6a30b
Marcus Heneborn <142923217+MarcusHNBRN@users.noreply.github.com>
2023-11-28 21:48:18 +0100

    commiting

--- flexboxscript.js
+++ flexboxscript.js
@@ -1,34 +1,42 @@
-let flexSlideIndex = 0;
-showFlexSlides(flexSlideIndex);
-
-function plusFlexSlides(n) {
-  showFlexSlides((flexSlideIndex += n));
+function isMobile() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 
-function currentFlexSlide(n) {
-  showFlexSlides((flexSlideIndex = n));
-}
+if (isMobile()) {
 
-function showFlexSlides(n) {
-  const slides = document.getElementsByClassName("product-slide-flexbox");
-  const details = document.querySelector(".details-text-flexbox");
 
-  details.style.display = "none";
+  let flexSlideIndex = 0;
+  showFlexSlides(flexSlideIndex);
 
-  if (n >= slides.length) {
-    flexSlideIndex = 0;
-  }
-  if (n < 0) {
-    flexSlideIndex = slides.length - 1;
+  function plusFlexSlides(n) {
+    showFlexSlides((flexSlideIndex += n));
   }
 
-  if (flexSlideIndex === 0) {
-    details.style.display = "block";
+  function currentFlexSlide(n) {
+    showFlexSlides((flexSlideIndex = n));
   }
 
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+  function showFlexSlides(n) {
+    const slides = document.getElementsByClassName("product-slide-flexbox");
+    const details = document.querySelector(".details-text-flexbox");
 
-  slides[flexSlideIndex].style.display = "block";
+    details.style.display = "none";
+
+    if (n >= slides.length) {
+      flexSlideIndex = 0;
+    }
+    if (n < 0) {
+      flexSlideIndex = slides.length - 1;
+    }
+
+    if (flexSlideIndex === 0) {
+      details.style.display = "block";
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    slides[flexSlideIndex].style.display = "block";
+  }
 }
