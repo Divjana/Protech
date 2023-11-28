d6e9d39d4520ce1c102ebb94da2a9e72b919f798
Divjana <divjanas@hotmail.se>
2023-11-28 15:41:35 +0100

    commit 1

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-/* Image cycle */
-
-var currentImageIndex = 0;
-var imagePaths = ["./images/fold.png", "./images/unfold.png"];
-
-var hasHovered = false;
-
-function startImageCycle() {
-  if (!hasHovered) {
-    hasHovered = true;
-    changePicture();
-  }
-}
-
-function changePicture() {
-  var image = document.getElementById("myImage");
-
-  currentImageIndex = (currentImageIndex + 1) % imagePaths.length;
-
-  image.src = imagePaths[currentImageIndex];
-
-  if (currentImageIndex < imagePaths.length - 1) {
-    setTimeout(changePicture, 2000);
-  } else {
-    hasHovered = false;
-  }
-}
-
-
 /* Button click */
 const mySubmitButton1 = document.querySelector('#myButton1');
 const emailInput1 = document.querySelector('#email1');
