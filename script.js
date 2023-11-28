7a5a2616e8aad6f1e76eb17feda8b85638c38aeb
Marcus Heneborn <heneborn@gmail.com>
2023-11-28 22:10:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,43 @@
-/* Image cycle */
+/* video */
 
-var currentImageIndex = 0;
-var imagePaths = ["./images/fold.png", "./images/unfold.png"];
+function toggleVideo(videoId) {
+  const clickedVideo = document.getElementById(videoId);
 
-var hasHovered = false;
+  const allVideos = document.querySelectorAll("video");
+  allVideos.forEach(function (video) {
+    if (video !== clickedVideo) {
+      video.pause();
+    }
+  });
 
-function startImageCycle() {
-  if (!hasHovered) {
-    hasHovered = true;
-    changePicture();
-  }
-}
-
-function changePicture() {
-  var image = document.getElementById("myImage");
-
-  currentImageIndex = (currentImageIndex + 1) % imagePaths.length;
-
-  image.src = imagePaths[currentImageIndex];
-
-  if (currentImageIndex < imagePaths.length - 1) {
-    setTimeout(changePicture, 2000);
+  if (clickedVideo.paused) {
+    clickedVideo.play();
   } else {
-    hasHovered = false;
+    clickedVideo.pause();
   }
 }
-
 
 /* Button click */
-const mySubmitButton1 = document.querySelector('#myButton1');
-const emailInput1 = document.querySelector('#email1');
+const mySubmitButton1 = document.querySelector("#myButton1");
+const emailInput1 = document.querySelector("#email1");
 
-mySubmitButton1.addEventListener('click', function(event) {
+mySubmitButton1.addEventListener("click", function (event) {
   event.preventDefault();
 
-  mySubmitButton1.textContent = 'Tack! Vi hör av oss!';
-  mySubmitButton1.style.backgroundColor = 'rgb(19, 201, 83)';
-  mySubmitButton1.style.color = 'black'; 
-  emailInput1.value = '';
+  mySubmitButton1.textContent = "Tack! Vi hör av oss!";
+  mySubmitButton1.style.backgroundColor = "rgb(19, 201, 83)";
+  mySubmitButton1.style.color = "black";
+  emailInput1.value = "";
 });
 
-const mySubmitButton2 = document.querySelector('#myButton2');
-const emailInput2 = document.querySelector('#email2');
+const mySubmitButton2 = document.querySelector("#myButton2");
+const emailInput2 = document.querySelector("#email2");
 
-mySubmitButton2.addEventListener('click', function(event) {
+mySubmitButton2.addEventListener("click", function (event) {
   event.preventDefault();
 
-  mySubmitButton2.textContent = 'Tack! Vi hör av oss!';
-  mySubmitButton2.style.backgroundColor = 'rgb(19, 201, 83)';
-  mySubmitButton2.style.color = 'black'; 
-  emailInput2.value = '';
+  mySubmitButton2.textContent = "Tack! Vi hör av oss!";
+  mySubmitButton2.style.backgroundColor = "rgb(19, 201, 83)";
+  mySubmitButton2.style.color = "black";
+  emailInput2.value = "";
 });
-
